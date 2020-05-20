00b28b75b2e46bc6954b9c563a80b4fe8fed04d4
Marguerite Blair <marguerite.blair@gmail.com>
2020-05-19 20:42:52 -0400

    notes on the introduction of the course

---
+++ introduction.js
@@ -0,0 +1,9 @@
+//dom == document object model
+//tree of elements/nodes 
+//the nodes represent every html element on your webpage
+//javascript lets us interact with the DOM
+//we can CHANGE html elements, add CSS styling, create new HTML elements, attach event listeners to elements
+
+
+//while jquery can be used for a lot of this stuff, we can use pure vanilla javascript
+//we'll be creating a mini reading list application
